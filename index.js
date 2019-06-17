3bda68c7984cdb387f29c6b596358992e0ed03b6
Jordan Vidrine <jordan@jordanvidrine.com>
2019-06-17 18:30:41 -0500

    css issues

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ let projects = document.getElementsByClassName('project')
 
   for (let count = 1; count <= projects.length; count++) {
     if (count%2 !== 0) {
-      projects[count-1].className = 'project odd';
+      projects[count-1].className += ' odd';
     }
   }
